1ce544c6b4db4b71d86e5dba070741f57e5bb5d7
Tuukka Grönholm <tmgronholm@gmail.com>
2023-11-17 18:01:08 +0200

    reseptit listattuna

--- src/main/java/com/kotikokkaaja/reseptivihko/web/ReseptiController.java
+++ src/main/java/com/kotikokkaaja/reseptivihko/web/ReseptiController.java
@@ -1,12 +1,27 @@
 package com.kotikokkaaja.reseptivihko.web;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.kotikokkaaja.reseptivihko.domain.ReseptiRepository;
 
 
 @Controller
 public class ReseptiController {
+	
+	// injektoidaan ReseptiRepository 
+	@Autowired 
+	private ReseptiRepository repository;
+	
+	// palauttaa sivun reseptilista ja listaa kaikki reseptit 
+	@GetMapping("/reseptilista")
+	public String reseptiList(Model model) {
+		model.addAttribute("reseptit", repository.findAll());
+		return "reseptilista";
+		}
 	
 	
 	
