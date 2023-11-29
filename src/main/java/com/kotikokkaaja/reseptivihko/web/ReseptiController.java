88159c276bbb695077cf8b874fff8f15549ddbf5
Tuukka Grönholm <tmgronholm@gmail.com>
2023-11-29 18:54:38 +0200

    poistettu turha importti

--- src/main/java/com/kotikokkaaja/reseptivihko/web/ReseptiController.java
+++ src/main/java/com/kotikokkaaja/reseptivihko/web/ReseptiController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kotikokkaaja.reseptivihko.domain.Resepti;
 import com.kotikokkaaja.reseptivihko.domain.ReseptiRepository;
